6129775e6355ae03dd3ece10c1ba74f60dd189e7
Ema <faunapescaycaza@gmail.com>
2026-02-16 00:47:13 -0300

    Add debug log to PrivateRoute

--- caza_2026_v2_frontend/src/components/PrivateRoute.js
+++ caza_2026_v2_frontend/src/components/PrivateRoute.js
@@ -3,7 +3,8 @@ import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext); // Added loading
+    console.log("PrivateRoute checking user:", user, "loading:", loading); // DEBUG
 
     if (loading) {
         return <div>Cargando...</div>; // O un spinner
